4a0f7809bb4acb9f2d76be83c1bfcea62e2c2d20
RyanOnTheInside <7623207+ryanontheinside@users.noreply.github.com>
2025-07-23 17:08:22 -0400

    remove extranneous exports

--- src/streamdiffusion/__init__.py
+++ src/streamdiffusion/__init__.py
@@ -2,22 +2,9 @@ from .pipeline import StreamDiffusion
 from .wrapper import StreamDiffusionWrapper
 from .config import load_config, save_config, create_wrapper_from_config
 
-# ControlNet support
 try:
     from .controlnet import (
         ControlNetPipeline,
-        create_controlnet_pipeline,
-        ControlNetConfig,
-        StreamDiffusionControlNetConfig,
-        load_config,
-        save_config,
-        create_wrapper_from_config,
-        BasePreprocessor,
-        CannyPreprocessor,
-        DepthPreprocessor,
-        OpenPosePreprocessor,
-        LineartPreprocessor,
-        get_preprocessor,
     )
 
     
@@ -28,12 +15,6 @@ try:
         "save_config",
         "create_wrapper_from_config",
         "ControlNetPipeline",
-        "BasePreprocessor",
-        "CannyPreprocessor",
-        "DepthPreprocessor", 
-        "OpenPosePreprocessor",
-        "LineartPreprocessor",
-        "get_preprocessor",
     ]
 except ImportError as e:
     print(f"Warning: ControlNet support not available: {e}")
