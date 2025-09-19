21574dde47fca593bb427c38472cda4a86f96bf0
RyanOnTheInside <7623207+ryanontheinside@users.noreply.github.com>
2025-09-19 16:16:09 -0400

    consolidate get_pipeline_class

--- demo/realtime-img2img/routes/common/dependencies.py
+++ demo/realtime-img2img/routes/common/dependencies.py
@@ -11,3 +11,9 @@ def get_app_instance():
     """Dependency to get the app instance - will be injected during router registration"""
     # This will be overridden when the router is included in main.py
     pass
+
+
+def get_pipeline_class():
+    """Dependency to get the Pipeline class - will be injected during router registration"""
+    # This will be overridden when the router is included in main.py
+    pass
